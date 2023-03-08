6085d9a3a6e6ad9a382f7dc5d48dd9d7da603f60
Maggus <mail4markus.schneider@gmx.de>
2023-03-08 22:25:49 +0100

    style: remove default contet

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <div className='text-3xl w-2 h-4 bg-indigo-500'>Header</div>{' '}
       </header>
-    </div>
+      <nav></nav>
+      <main></main>
+      <footer></footer>
+    </>
   );
 }
 
